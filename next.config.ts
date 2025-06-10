fc1127806d0933cfbbc45a82e7ef5288272df550
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2025-06-10 13:39:57 +0100

    test server next

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lhdev.com.br'],
   },
-  allowedDevOrigins: ['https://admin.lhdev.com.br:3000'],
+  allowedDevOrigins: ['https://admin.lhdev.com.br:3000', 'https://restaurante-spumoni.vercel.app/admin'],
 
 };
 
