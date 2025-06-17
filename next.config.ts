782d94daa568cddb09011e5b3ce6bac291da93eb
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2025-06-17 22:35:59 +0100

    refactor next config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lhdev.com.br', 'restaurantespumoni.com.br'],
   },
-  allowedDevOrigins: ['https://admin.lhdev.com.br:3000', 'https://restaurante-spumoni.vercel.app/admin'],
+  allowedDevOrigins: ['https://admin.lhdev.com.br:3000', 'https://site.restaurantespumoni.com.br/admin'],
 
 };
 
