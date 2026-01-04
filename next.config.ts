a2283613a5596ac4c58ca4bd99c37dcf12b10d12
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2026-01-04 19:28:08 +0000

    change type of pedido

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['lhdev.com.br', 'restaurantespumoni.com.br'],
+    domains: ["lhdev.com.br", "restaurantespumoni.com.br"],
   },
-  allowedDevOrigins: ['https://admin.lhdev.com.br:3000', 'https://site.restaurantespumoni.com.br/admin'],
-
+  allowedDevOrigins: [
+    "https://admin.lhdev.com.br:3000",
+    "https://site.restaurantespumoni.com.br/admin",
+    "https://site.restaurantespumoni.com.br",
+  ],
 };
 
 export default nextConfig;
