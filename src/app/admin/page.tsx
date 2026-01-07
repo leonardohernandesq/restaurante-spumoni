d67ad9d1babb237060038efe8782ebc40fa56240
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2026-01-07 20:06:21 +0000

    feat: hide and show password

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,61 +1,105 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { BsArrowRight } from 'react-icons/bs'
-import { toast } from 'react-toastify'
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { BsArrowRight } from "react-icons/bs";
+import { toast } from "react-toastify";
 
-import { Container } from '@/components/Container'
-import { userStore } from '@/store/userStore';
-import { LoadingIcon } from '@/components/LoadingIcon'
+import { Container } from "@/components/Container";
+import { userStore } from "@/store/userStore";
+import { LoadingIcon } from "@/components/LoadingIcon";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const { login, loading } = userStore();
-    const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const { login, loading } = userStore();
+  const router = useRouter();
 
-    const handleLogin = async () => {
-        if (!email || !senha) {
-            toast.error('Verifique os dados informados');
-            return;
-        }
+  const handleLogin = async () => {
+    if (!email || !senha) {
+      toast.error("Verifique os dados informados");
+      return;
+    }
 
-        try {
-            await login(email, senha);
+    try {
+      await login(email, senha);
 
-            toast.success('Login realizado com sucesso!');
+      toast.success("Login realizado com sucesso!");
 
-            router.push('/admin/pedidos')
-        } catch (err: unknown) {
-            let serverMsg = 'Erro ao fazer login';
+      router.push("/admin/pedidos");
+    } catch (err: unknown) {
+      let serverMsg = "Erro ao fazer login";
 
-            if (typeof err === 'object' && err !== null && 'response' in err) {
-                const error = err as { response?: { data?: { message?: string } } };
-                serverMsg = error.response?.data?.message || serverMsg;
-            }
+      if (typeof err === "object" && err !== null && "response" in err) {
+        const error = err as { response?: { data?: { message?: string } } };
+        serverMsg = error.response?.data?.message || serverMsg;
+      }
 
-            toast.error(serverMsg);
-        }
+      toast.error(serverMsg);
+    }
+  };
 
-    };
+  return (
+    <Container styleRow="bg-green-principal-500">
+      <main className="flex flex-col items-center justify-center min-h-screen px-7 py-5 ">
+        <Link href={"/"}>
+          <Image
+            src={"logo-footer.svg"}
+            alt="Logo do Restaurante Spumoni"
+            width={300}
+            height={100}
+          />
+        </Link>
+        <section className="flex flex-col text-center text-white my-10 bg-green-principal-900 p-8 w-full max-w-lg gap-5 rounded-3xl shadow-2xl">
+          <h1 className="text-5xl font-bold">Login</h1>
+          <h2 className="">Acesse sua lista de pedidos.</h2>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            className="bg-green-950 py-2.5 px-5 placeholder:text-zinc-400 rounded-xl"
+            placeholder="Digite seu usuário: "
+          />
+          <div className="relative w-full flex items-center">
+            <input
+              type={showPassword ? "text" : "password"}
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              className="bg-green-950 py-2.5 px-5 placeholder:text-zinc-400 rounded-xl w-full"
+              placeholder="Digite sua senha: "
+            />
+            {showPassword ? (
+              <FiEyeOff
+                className="absolute right-5 cursor-pointer text-zinc-400"
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            ) : (
+              <FiEye
+                className="absolute right-5 cursor-pointer text-zinc-400"
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            )}
+          </div>
+          <button
+            className="bg-purple-principal-700 py-2.5 px-5 rounded-xl font-medium flex items-center justify-center gap-3 cursor-pointer hover:bg-purple-principal-900 transition-colors"
+            onClick={() => handleLogin()}
+          >
+            {loading ? (
+              <LoadingIcon />
+            ) : (
+              <>
+                LOGIN <BsArrowRight />
+              </>
+            )}{" "}
+          </button>
+        </section>
+      </main>
+    </Container>
+  );
+};
 
-    return (
-        <Container styleRow='bg-green-principal-500'>
-            <main className='flex flex-col items-center justify-center min-h-screen px-7 py-5 '>
-                <Link href={'/'}><Image src={'logo-footer.svg'} alt='Logo do Restaurante Spumoni' width={300} height={100} /></Link>
-                <section className='flex flex-col text-center text-white my-10 bg-green-principal-900 p-8 w-full max-w-lg gap-5 rounded-3xl shadow-2xl'>
-                    <h1 className='text-5xl font-bold'>Login</h1>
-                    <h2 className=''>Acesse sua lista de pedidos.</h2>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" className='bg-green-950 py-2.5 px-5 placeholder:text-zinc-400 rounded-xl' placeholder='Digite seu usuário: ' />
-                    <input value={senha} onChange={(e) => setSenha(e.target.value)} type="text" className='bg-green-950 py-2.5 px-5 placeholder:text-zinc-400 rounded-xl' placeholder='Digite sua senha: ' />
-                    <button className='bg-purple-principal-700 py-2.5 px-5 rounded-xl font-medium flex items-center justify-center gap-3 cursor-pointer' onClick={() => handleLogin()}>{loading ? <LoadingIcon /> : <>LOGIN <BsArrowRight /></>} </button>
-                </section>
-            </main>
-        </Container>
-    )
-}
-
-export default Login
+export default Login;
