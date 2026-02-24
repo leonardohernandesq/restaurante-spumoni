90fbb794eed75db761359d6b3edc13b77dc83ca4
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2026-02-24 00:27:36 +0000

    feat: create recovery pass

--- src/app/alterar-senha/page.tsx
+++ src/app/alterar-senha/page.tsx
@@ -1,11 +1,25 @@
 // app/alterar-senha/page.tsx
 import ResetPasswordForm from "./ResetPasswordForm";
 
-export default function AlterarSenhaPage({
-  searchParams,
-}: {
-  searchParams: { code?: string };
-}) {
-  const code = searchParams.code || "";
+interface PageProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function AlterarSenhaPage({ searchParams }: PageProps) {
+  // Await the searchParams promise
+  const resolvedParams = await searchParams;
+  const code =
+    typeof resolvedParams.code === "string" ? resolvedParams.code : "";
+
+  if (!code) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-red-500 font-bold">
+          Token não encontrado ou inválido!
+        </p>
+      </div>
+    );
+  }
+
   return <ResetPasswordForm code={code} />;
 }
