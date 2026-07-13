dc8b7346f920192fcd56ca6d03ac98e1168187f2
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2026-07-14 00:43:45 +0100

    fix: edit frete

--- src/services/frete.ts
+++ src/services/frete.ts
@@ -39,7 +39,7 @@ export const freteService = {
 
   // Atualizar frete por ID
   async update(id: number, payload: Frete): Promise<void> {
-    await api.put(`/frete/edit`, payload);
+    await api.put(`/frete/edit?id=${id}`, payload);
   },
 
   // Deletar frete por ID
